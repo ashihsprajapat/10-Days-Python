ba9e94c101a40f2e1af5e9f59a7a4af54ba859f2
ashish prajapat <ashishprajapat507@gmail.com>
2025-07-04 08:12:27 +0530

    lecture 2 Conditionstatement and String

---
+++ String & Condition statement/ConditionStatement.py
@@ -0,0 +1,10 @@
+
+age= int( input("Enter the age"))
+
+if(age >=18):
+    print("can add in voting list")
+    print("Congratulation you can get licence")
+elif(age>=45):
+    print("your can get pention ")
+else:
+    print("you are yong so can get job in google")
---
+++ String & Condition statement/String.py
@@ -0,0 +1,38 @@
+
+name="Ferheen"
+print(name)
+
+# some methods of string
+#Concatination
+
+first="Ferheen"
+last="Feroz"
+full=first +" "+ last
+
+print(full)
+
+#length
+print(len(full))
+
+#Index
+print(full[3])
+
+# find method
+print(full.find("f"))  #retunr index of first occurence outher wise return -1
+
+#endswith
+print(full.endswith("z")) # return Boolean value
+
+# count method
+print(full.count("e")) # return the count of occurence
+
+# Capitalize method
+print(full.capitalize()) # do first character is capital
+
+#upper method
+print(full.upper())  # do all character in capital
+
+print(full.lower()) # do lower all characters
+
+# name=input("enter your name")
+# print(len(name), name)
